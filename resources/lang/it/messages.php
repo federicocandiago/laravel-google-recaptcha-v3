5da40d22899ec2e8f969ed2b031b7a5d69b5bdd1
federicocandiago <33969447+federicocandiago@users.noreply.github.com>
2020-11-17 20:49:06 +0100

    Create messages.php

---
+++ resources/lang/it/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ERROR_MISSING_INPUT' => 'Risposta dell\input mancante.',
+    'ERROR_UNABLE_TO_VERIFY' => 'Impossibile verificare.',
+    'ERROR_HOSTNAME' => 'L\'hostname non corrisponde.',
+    'ERROR_ACTION' => 'L\'azione non corrispone',
+    'ERROR_SCORE_THRESHOLD' => 'Non è possibile verificare la richiesta',
+    'ERROR_TIMEOUT' => 'Timeout',
+    'SUCCESS' => 'Operazione completata con successo',
+    'TIMEOUT_OR_DUPLICATE'=> 'Tempo scaduto, è necessario rieffettuare il login',
+];
